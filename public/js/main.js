ffc94c21e2679af9d0048177754607e37609148e
SakuyaYae <darkreimu@outlook.com>
2023-11-27 15:35:31 +0100

    added more test functionalety

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,11 @@
-import { getAllProperties, addProperty, login, getOneProperty, updateUser } from "./function/server-request.js";
+import { getAllProperties, addProperty, login, getOneProperty, getAllGeneralMsg, getAllSaleMsg, updateUser } from "./function/server-request.js";
 
 async function main() {
-  document.getElementById("display").innerText = await getAllProperties();
   console.log(await getAllProperties());
   console.log(await login({ "userName": "Sakuya", "pass": "4444" }));
-  console.log(await addProperty({ home: " house" }));
-  console.log(await getOneProperty(1));
+  //console.log(await addProperty({ home: " house" }));
+  console.log(await getOneProperty(2));
+  console.log(await getAllGeneralMsg());
+  console.log(await getAllSaleMsg());
 }
 main()
