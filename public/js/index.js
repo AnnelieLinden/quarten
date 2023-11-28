d6e9a873ee786c624b85878e3d0db9068f2dfe3b
AnnelieLinden <lia.akesson@gmail.com>
2023-11-28 15:26:57 +0100

    start på saleformulär

--- public/js/index.js
+++ public/js/index.js
@@ -4,22 +4,24 @@ const content = document.querySelector('#content')
 onhashchange = changePage;
 changePage()
 function changePage() {
+  document.querySelector('main').textContent = ""
+
   console.log('my page will be', location.hash);
   if (location.hash === "#homepage") {
     renderHomepage();
   } else if (location.hash === "#sale") {
-    document.querySelector('main').innerHTML = '<h1>This is sale page</h1>';
+    renderSalePage()
   } else if (location.hash === "#buy") {
-    document.querySelector('main').innerHTML = '<h1>This is buy page</h1>';
+    document.querySelector('main');
   } else if (location.hash === "#login") {
-    document.querySelector('main').innerHTML = '<h1>This is login page</h1>';
+    document.querySelector('main');
   } else {
-    document.querySelector('main').innerHTML = '<h1>This is contact us page</h1>';
+    document.querySelector('main');
   }
 }
 
 function renderHomepage() {
-    const saleButton = document.createElement('a')
+  const saleButton = document.createElement('a')
   saleButton.setAttribute('href', '#sale')
   const linkText2 = document.createTextNode("Sälj");
   saleButton.appendChild(linkText2);
@@ -30,6 +32,44 @@ function renderHomepage() {
   const linkText = document.createTextNode("Köp");
   buyButton.appendChild(linkText);
   content.appendChild(buyButton)
+}
+function renderSalePage() {
+  const form = document.createElement('form')
+  form.setAttribute("id", "saleForm")
+  const propertyLabel = document.createElement('label')
+  propertyLabel.innerText = "Fastighetsbeteckning:"
+  const propertyInput = document.createElement('input')
+  const buildingTypeLabel = document.createElement('label')
+  buildingTypeLabel.innerText = "Bostadstyp:"
+  const apartmentType = document.createElement('input')
+  const villaType = document.createElement('input')
+  apartmentType.setAttribute('type', 'radio', 'name', 'apartmentType', 'value', 'Bostadsrätt')
+  //const apartmentTypeLabel = cosument.createElement('label')
+  //apartmentTypeLabel.innerText = "Bostadsrätt"
+  villaType.setAttribute('type', 'radio', 'name', 'villaType', 'value', 'Villa')
+  //const villaTypeLabel = cosument.createElement('label')
+  //vilaTypeLabel.innerText = "Villa"
+  const spaceLabel = document.createElement('label')
+  spaceLabel.innerText = "Bostadsyta:"
+  const spaceInput = document.createElement('input')
+  const elevatorLabel = document.createElement('label')
+  elevatorLabel.innerText = "Hiss" 
+  const elevatorInput = document.createElement('input')
+  elevatorInput.setAttribute('type', 'radio', 'name', 'elevator','value', 'Ja')
+  elevatorInput.setAttribute('type', 'radio', 'name', 'elevator','value', 'Nej')
+  form.appendChild(propertyLabel)  
+  form.appendChild(propertyInput)  
+  form.appendChild(buildingTypeLabel)
+  form.appendChild(apartmentType)
+  form.appendChild(villaType)
+  //form.appendChild(apartmentTypeLabel)
+  //form.appendChild(villaTypeLabel)
+  form.appendChild(spaceLabel)
+  form.appendChild(spaceInput)
+  form.appendChild(elevatorLabel) 
+  form.appendChild(elevatorInput)
+
+  content.appendChild(form)
 }
 
 
