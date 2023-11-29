4dd1e54ba58b585d9424051c1c1cb2053243b222
SakuyaYae <darkreimu@outlook.com>
2023-11-29 10:57:58 +0100

    minor cleaning

--- public/js/function/createForm.js
+++ public/js/function/createForm.js
@@ -7,15 +7,14 @@ export default function createLoginForm() {
   form.appendChild(inputWhitLableArrUname[0]);
   form.appendChild(inputWhitLableArrUname[1]);
 
-
   var inputWhitLableArrPass = createInputWhitLabel("pass");
   form.appendChild(inputWhitLableArrPass[0]);
   form.appendChild(inputWhitLableArrPass[1]);
 
-
   const button = document.createElement("button");
   const userName = inputWhitLableArrUname[1];
   const pass = inputWhitLableArrPass[1];
+
   button.innerText = "Submit";
   button.addEventListener("click", async function (event) {
     event.preventDefault();
@@ -24,7 +23,8 @@ export default function createLoginForm() {
       "pass": pass.value
     }
     console.log(await login(user));
-  })
+  });
+
   form.appendChild(button);
   return form;
 }
