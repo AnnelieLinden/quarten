11b17d542b5cc06a556a14e1847fbabdc7e65e37
SakuyaYae <darkreimu@outlook.com>
2023-11-29 10:57:11 +0100

    added more testing functonalety

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-import { getAllProperties, addProperty, login, getOneProperty, getAllGeneralMsg, getAllSaleMsg, updateUser } from "./function/server-request.js";
+import { getAllProperties, addProperty, addGenaralForm, login, getOneProperty, getAllGeneralMsg, getAllSaleMsg, addSaleForm, updateUser } from "./function/server-request.js";
 import createForm from "./function/createForm.js";
 
 async function main() {
@@ -7,6 +7,18 @@ async function main() {
   console.log(await login({ "userName": "Yukina", "pass": "4444" }));
   console.log(await login({ "userName": "Sakuya", "pass": "0000" }));
   //console.log(await addProperty({ home: " house" }));
+  await addSaleForm({
+    "phone": "07000000",
+    "email": "ex.ex@ex.ex",
+    "firstName": "____",
+    "lastName": "-----",
+    "home": "house"
+  })
+  await addGenaralForm({
+    "firstName": "____",
+    "lastName": "-----",
+    "req": "xxx"
+  })
   console.log(await getOneProperty(2));
   console.log(await getAllGeneralMsg());
   console.log(await getAllSaleMsg());
