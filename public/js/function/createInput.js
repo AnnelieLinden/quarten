c99f5130227f3d45e653ef064b5c73f65f20297a
SakuyaYae <darkreimu@outlook.com>
2023-11-29 10:58:14 +0100

    minor cleaning

--- public/js/function/createInput.js
+++ public/js/function/createInput.js
@@ -2,12 +2,15 @@ export default function createInputWhitLabel(idValue = "default") {
   const returnArr = [];
   const input = document.createElement("input");
   const lable = document.createElement("label");
+
   if (idValue === "default") {
     idValue += "_0"
   }
+
   lable.setAttribute("for", idValue);
   lable.innerText = idValue;
   input.id = idValue;
+
   returnArr.push(lable);
   returnArr.push(input);
   return returnArr;
