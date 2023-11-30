f8daf6ec77f3f3738416304d7f72e10495ed00c9
LottaSvanhall <charlotte.svanhall@live.se>
2023-11-30 15:51:24 +0100

    Added checkboxes
    Added checkboxes for propertys.

--- public/js/findProperty.js
+++ public/js/findProperty.js
@@ -1,15 +1,58 @@
 export default function findProperty(buyForm) {
 
+  const pTag = document.createElement('p')
+  pTag.setAttribute('id', 'pTag')
+  pTag.innerText = "Hitta din bostad!"
 
   buyForm.setAttribute("id", "buyForm")
 
   const propertyLabel = document.createElement('label')
-  propertyLabel.innerText = "Bostadstyp:"
+  propertyLabel.innerText = "Välj Bostadstyp:"
   const propertyInput = document.createElement('input')
+
+
+  const villaLabel = document.createElement('label')
+  const villaLabel1 = document.createElement('label')
+  villaLabel.innerText = "Villa:"
+  villaLabel1.innerText = "Ja"
+  const villaInput = document.createElement('input')
+  villaInput.setAttribute('type', 'checkbox')
+  villaInput.setAttribute('name', 'villa')
+  villaInput.setAttribute('value', 'checked')
+  
+  const apartmentLabel = document.createElement('label')
+  const apartmentLabel1 = document.createElement('label')
+  apartmentLabel.innerText = "Bostadsrätt:"
+  apartmentLabel1.innerText = "Ja"
+  const apartmentInput = document.createElement('input')
+  apartmentInput.setAttribute('type', 'checkbox')
+  apartmentInput.setAttribute('name', 'apartment')
+  apartmentInput.setAttribute('value', 'checked')
+
+//radioknapp
+  /*const apartmentInput = document.createElement('input')
+  const villaInput = document.createElement('input')
+
+  apartmentInput.setAttribute('type', 'radio')
+  apartmentInput.setAttribute('name', 'propertyType')
+  apartmentInput.setAttribute('value', 'Bostadsrätt')
+  const apartmentInputLabel = document.createElement('label')
+  apartmentInputLabel.innerText = "Bostadsrätt:"
+
+  villaInput.setAttribute('type', 'radio')
+  villaInput.setAttribute('name', 'propertyType')
+  villaInput.setAttribute('value', 'Villa')
+  const villaInputLabel = document.createElement('label')
+  villaInputLabel.innerText = "Villa:"*/
+
+
 
   const neighborhoodLabel = document.createElement('label') //bostadsområde
   neighborhoodLabel.innerText = "Bostadsområde:"
   const neighborhoodInput = document.createElement('input')
+
+
+  
 
   const priceLabel = document.createElement('label')
   priceLabel.innerText = "Utgångspris:"
@@ -19,8 +62,21 @@ export default function findProperty(buyForm) {
   areaLabel.innerText = "Bostadsyta:"
   const areaInput = document.createElement('input')
 
+  buyForm.appendChild(pTag)
   buyForm.appendChild(propertyLabel)
   buyForm.appendChild(propertyInput)
+  
+//radioknapp
+ /* buyForm.appendChild(apartmentInputLabel)
+  buyForm.appendChild(apartmentInput)
+  buyForm.appendChild(villaInputLabel)
+  buyForm.appendChild(villaInput)*/
+  
+ 
+  buyForm.appendChild(villaLabel)
+  buyForm.appendChild(villaInput)
+  buyForm.appendChild(apartmentLabel)
+  buyForm.appendChild(apartmentInput)
   buyForm.appendChild(neighborhoodLabel)
   buyForm.appendChild(neighborhoodInput)
   buyForm.appendChild(priceLabel)
