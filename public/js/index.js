4ed5565f898a2b850cc564833a2773886cd14972
Noddan84 <146703809+Noddan84@users.noreply.github.com>
2023-12-07 09:18:41 +0100

    changes homepage
    lite styling och text

--- public/js/index.js
+++ public/js/index.js
@@ -21,6 +21,7 @@ function changePage() {
   console.log('my page will be', location.hash);
   if (location.hash === "#homepage") {
     renderHomepage();
+    textHomepage();
   } else if (location.hash === "#sale") {
     const saleForm = document.createElement('form')
     renderSaleForm(saleForm)
@@ -32,7 +33,7 @@ function changePage() {
 
   } else if (location.hash === "#contact-us") {
     generalForm()
- 
+
   } else if (location.hash === "#about-us") {
     aboutUs()
   }
@@ -54,4 +55,28 @@ function renderHomepage() {
   buyButton.appendChild(linkText);
   content.appendChild(buyButton)
 }
+
+  function textHomepage() {
+    const homepageBr = document.createElement('br');
+    const homeSection = document.createElement('section');
+    const homeSection2 = document.createElement('section');
+    const homePtext = document.createElement('homePtext');
+    const homeP2text = document.createElement('homeP2text');
+    homeSection.setAttribute('id', 'homeSection');
+    homeSection2.setAttribute('id', 'homeSection2');
+    homepageBr.setAttribute('id', 'homepageBr');
+    homePtext.setAttribute('id', 'homePtext');
+    homeP2text.setAttribute('id', 'homeP2text');
+    homePtext.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    homeP2text.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    homeSection.appendChild(homePtext);
+    homeSection.appendChild(homepageBr);
+    content.appendChild(homeSection);
+    content.appendChild(homepageBr);
+    homeSection2.appendChild(homeP2text);
+    homeSection2.appendChild(homepageBr);
+    content.appendChild(homeSection2);
+    content.appendChild(homepageBr);
+  }
+
 
